608c38619cb608427efeeeebcf1f44b8648168ff
M Newman <megan.m.boggs@gmail.com>
2022-11-23 14:03:49 -0800

    update files from cat to dog

--- index.js
+++ index.js
@@ -1,90 +1,88 @@
-import { catsData } from '/data.js'
+import { pupsData } from "/data.js";
 
-const emotionRadios = document.getElementById('emotion-radios')
-const getImageBtn = document.getElementById('get-image-btn')
-const gifsOnlyOption = document.getElementById('gifs-only-option')
-const memeModalInner = document.getElementById('meme-modal-inner')
-const memeModal = document.getElementById('meme-modal')
-const memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
+const emotionRadios = document.getElementById("emotion-radios");
+const getImageBtn = document.getElementById("get-image-btn");
+const gifsOnlyOption = document.getElementById("gifs-only-option");
+const memeModalInner = document.getElementById("meme-modal-inner");
+const memeModal = document.getElementById("meme-modal");
+const memeModalCloseBtn = document.getElementById("meme-modal-close-btn");
 
-emotionRadios.addEventListener('change', highlightCheckedOption)
+emotionRadios.addEventListener("change", highlightCheckedOption);
 
-memeModalCloseBtn.addEventListener('click', closeModal)
+memeModalCloseBtn.addEventListener("click", closeModal);
 
-getImageBtn.addEventListener('click', renderCat)
+getImageBtn.addEventListener("click", renderPup);
 
-function highlightCheckedOption(e){
-    const radios = document.getElementsByClassName('radio')
-    for (let radio of radios){
-        radio.classList.remove('highlight')
-    }
-    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+function highlightCheckedOption(e) {
+  const radios = document.getElementsByClassName("radio");
+  for (let radio of radios) {
+    radio.classList.remove("highlight");
+  }
+  document.getElementById(e.target.id).parentElement.classList.add("highlight");
 }
 
-function closeModal(){
-    memeModal.style.display = 'none'
+function closeModal() {
+  memeModal.style.display = "none";
 }
 
-function renderCat(){
-    const catObject = getSingleCatObject()
-    memeModalInner.innerHTML =  `
+function renderPup() {
+  const pupObject = getSinglePupObject();
+  memeModalInner.innerHTML = `
         <img 
-        class="cat-img" 
-        src="./images/${catObject.image}"
-        alt="${catObject.alt}"
+        class="pup-img" 
+        src="./images/${pupObject.image}"
+        alt="${pupObject.alt}"
         >
-        `
-    memeModal.style.display = 'flex'
+        `;
+  memeModal.style.display = "flex";
 }
 
-function getSingleCatObject(){
-    const catsArray = getMatchingCatsArray()
-    
-    if(catsArray.length === 1){
-        return catsArray[0]
+function getSinglePupObject() {
+  const pupsArray = getMatchingPupsArray();
+
+  if (pupsArray.length === 1) {
+    return pupsArray[0];
+  } else {
+    const randomNumber = Math.floor(Math.random() * pupsArray.length);
+    return pupsArray[randomNumber];
+  }
+}
+
+function getMatchingPupsArray() {
+  if (document.querySelector('input[type="radio"]:checked')) {
+    const selectedEmotion = document.querySelector(
+      'input[type="radio"]:checked'
+    ).value;
+    const isGif = gifsOnlyOption.checked;
+
+    const matchingPupsArray = pupsData.filter(function (pup) {
+      if (isGif) {
+        return pup.emotionTags.includes(selectedEmotion) && pup.isGif;
+      } else {
+        return pup.emotionTags.includes(selectedEmotion);
+      }
+    });
+    return matchingPupsArray;
+  }
+}
+
+function getEmotionsArray(pups) {
+  const emotionsArray = [];
+  for (let pup of pups) {
+    for (let emotion of pup.emotionTags) {
+      if (!emotionsArray.includes(emotion)) {
+        emotionsArray.push(emotion);
+      }
     }
-    else{
-        const randomNumber = Math.floor(Math.random() * catsArray.length)
-        return catsArray[randomNumber]
-    }
+  }
+  return emotionsArray;
 }
 
-function getMatchingCatsArray(){     
-    if(document.querySelector('input[type="radio"]:checked')){
-        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
-        const isGif = gifsOnlyOption.checked
-        
-        const matchingCatsArray = catsData.filter(function(cat){
-            
-            if(isGif){
-                return cat.emotionTags.includes(selectedEmotion) && cat.isGif
-            }
-            else{
-                return cat.emotionTags.includes(selectedEmotion)
-            }            
-        })
-        return matchingCatsArray 
-    }  
-}
-
-function getEmotionsArray(cats){
-    const emotionsArray = []    
-    for (let cat of cats){
-        for (let emotion of cat.emotionTags){
-            if (!emotionsArray.includes(emotion)){
-                emotionsArray.push(emotion)
-            }
-        }
-    }
-    return emotionsArray
-}
-
-function renderEmotionsRadios(cats){
-        
-    let radioItems = ``
-    const emotions = getEmotionsArray(cats)
-    for (let emotion of emotions){
-        radioItems += `
+function renderEmotionsRadios(pups) {
+  let radioItems = ``;
+  const emotions = getEmotionsArray(pups);
+  for (let emotion of emotions) {
+    radioItems += `
         <div class="radio">
             <label for="${emotion}">${emotion}</label>
             <input
@@ -93,13 +91,9 @@ function renderEmotionsRadios(cats){
             value="${emotion}"
             name="emotions"
             >
-        </div>`
-    }
-    emotionRadios.innerHTML = radioItems
+        </div>`;
+  }
+  emotionRadios.innerHTML = radioItems;
 }
 
-renderEmotionsRadios(catsData)
-
-
-
-
+renderEmotionsRadios(pupsData);
